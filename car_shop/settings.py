7aff8e427b0647208b40d5f2370473260f7fbb33
erastov <fedor_erastov@mail.ru>
2017-10-13 13:50:50 +0300

    Little edit

--- car_shop/settings.py
+++ car_shop/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
